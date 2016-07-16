cb6d7016354c9bd0b81e9e197e3bc46bbb7b1908
Tamara Piksa <tamara.piksa@gmail.com>
2016-07-16 10:57:15 -0700

    Render cat component

--- script.js
+++ script.js
@@ -20,3 +20,33 @@ describe('Basic Example', function() {
     expect(element).not.toBe(null);
   });
 });
+
+
+class Cats extends React.Component {
+  render() {
+    return <ul id="my-cats">
+      <li>Chia</li>
+      <li>Flaco</li>
+    </ul>
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return <div>
+      <Cats />
+      <p>These are my cats!</p>
+    </div>
+  }
+}
+
+ReactDOM.render(<Header />, document.getElementById('app'));
+
+
+
+describe('Exercise Tests', function() {
+  it('should render the right component', () => {
+    const element = document.getElementById('my-cats');
+    expect(element).not.toBe(null);
+  });
+});
